67596bea62dd34c35045be406b85507bb6da8aef
JaredR0 <128436390+JaredR0@users.noreply.github.com>
2023-05-08 13:55:02 -0600

    Update Estaci_n_de_automatizaci_n.ino

--- Estaci_n_de_automatizaci_n.ino
+++ Estaci_n_de_automatizaci_n.ino
@@ -220,36 +220,10 @@ void loop() {
 
 void temperaturaVerde(){
 if(temp > 21 && temp < 25){
-   lcd_JARED.clear();
    lcd_JARED.setCursor(0,0);
-   lcd_JARED.write(1);
-   lcd_JARED.setCursor(0,1);
-   lcd_JARED.write(1);
-   lcd_JARED.setCursor(1,0);
-   lcd_JARED.write(2);
-   lcd_JARED.setCursor(1,1);
-   lcd_JARED.write(2);
-   delay(100);
-   lcd_JARED.setCursor(2,0);
-   lcd_JARED.write(2);
-   lcd_JARED.setCursor(2,1);
-   lcd_JARED.write(2);
-   delay(100);
-   lcd_JARED.setCursor(3,0);
-   lcd_JARED.write(2);
-   lcd_JARED.setCursor(3,1);
-   lcd_JARED.write(2);
-   delay(100);
+   lcd_JARED.print("Temp");
    lcd_JARED.setCursor(4,0);
-   lcd_JARED.write(2);
-   lcd_JARED.setCursor(4,1);
-   lcd_JARED.write(2);
-   delay(100);
-   lcd_JARED.setCursor(5,0);
-   lcd_JARED.write(3);
-   lcd_JARED.setCursor(5,1);
-   lcd_JARED.write(3);
-   delay(250);
+   lcd_JARED.write(5);
    digitalWrite(Verde, HIGH);
    digitalWrite(Rojo,LOW);
    digitalWrite(Azul,LOW);
@@ -257,41 +231,10 @@ if(temp > 21 && temp < 25){
 }
 void temperaturaRojo(){
   if(temp > 25 && temp < 45){
-   lcd_JARED.clear();
    lcd_JARED.setCursor(0,0);
-   lcd_JARED.write(1);
-   lcd_JARED.setCursor(15,0);
-   lcd_JARED.write(1);
-   lcd_JARED.setCursor(1,0);
-   lcd_JARED.write(2);
-   lcd_JARED.setCursor(14,0);
-   lcd_JARED.write(2);
-   delay(100);
-   lcd_JARED.setCursor(2,0);
-   lcd_JARED.write(2);
-   lcd_JARED.setCursor(13,0);
-   lcd_JARED.write(2);
-   delay(100);
-   lcd_JARED.setCursor(3,0);
-   lcd_JARED.write(2);
-   lcd_JARED.setCursor(12,0);
-   lcd_JARED.write(2);
-   delay(100);
+   lcd_JARED.print("Temp");
    lcd_JARED.setCursor(4,0);
-   lcd_JARED.write(2);
-   lcd_JARED.setCursor(11,0);
-   lcd_JARED.write(2);
-   delay(100);
-   lcd_JARED.setCursor(5,0);
-   lcd_JARED.write(3);
-   lcd_JARED.setCursor(10,0);
-   lcd_JARED.write(3);
-   delay(100);
-   lcd_JARED.setCursor(7,0);
-   lcd_JARED.write(5);
-   lcd_JARED.setCursor(8,0);
-   lcd_JARED.write(4);
-   delay(250);
+   lcd_JARED.write(6);
    digitalWrite(Rojo,HIGH);
    digitalWrite(Verde,LOW);
    digitalWrite(Azul,LOW);
@@ -299,28 +242,10 @@ void temperaturaRojo(){
 }
 void temperaturaMorado(){
   if(temp > 15 && temp < 21 ){
-   lcd_JARED.clear();
    lcd_JARED.setCursor(0,0);
-   lcd_JARED.write(1);
-   lcd_JARED.setCursor(1,0);
-   lcd_JARED.write(6);
-   lcd_JARED.setCursor(6,0);
-   lcd_JARED.write(8);
-   delay(100);
-   lcd_JARED.setCursor(2,0);
-   lcd_JARED.write(6);
-   delay(100);
-   lcd_JARED.setCursor(3,0);
-   lcd_JARED.write(6);
-   delay(100);
+   lcd_JARED.print("Temp");
    lcd_JARED.setCursor(4,0);
-   lcd_JARED.write(6);
-   delay(100);
-   lcd_JARED.setCursor(5,0);
    lcd_JARED.write(7);
-   lcd_JARED.setCursor(6,0);
-   lcd_JARED.write(8);
-   delay(250);
   digitalWrite(Rojo,HIGH);
   digitalWrite(Azul,HIGH);
   digitalWrite(Verde,LOW);
@@ -328,6 +253,10 @@ void temperaturaMorado(){
   }
   void sensoradar(bool Detection){
     if(Detection == HIGH){
+    lcd_JARED.setCursor(7,0);
+    lcd_JARED.print("Detect");
+    lcd_JARED.setCursor(13,0);
+    lcd_JARED.write(8);
     for (int i = 0; i < NUM_LEDS; i++) {
     pixels.setPixelColor(i, pixels.Color(255, 0, 255));  
     pixels.show();                                     
@@ -339,44 +268,11 @@ void temperaturaMorado(){
     delay(100);
     }
     tone(BUZZER,1500);
- 
-   delay(100);
-   lcd_JARED.clear();
-   lcd_JARED.setCursor(2,1);
-   lcd_JARED.print("hay alguien");
-   lcd_JARED.setCursor(0,0);
-   lcd_JARED.write(7);
-   lcd_JARED.setCursor(15,0);
-   lcd_JARED.write(7);
-   delay(100);
-   lcd_JARED.setCursor(2,0);
-   lcd_JARED.write(7);
-   lcd_JARED.setCursor(13,0);
-   lcd_JARED.write(7);
-   delay(100);
-   lcd_JARED.setCursor(3,0);
-   lcd_JARED.write(7);
-   lcd_JARED.setCursor(12,0);
-   lcd_JARED.write(7);
-   delay(100);
-   lcd_JARED.setCursor(4,0);
-   lcd_JARED.write(7);
-   lcd_JARED.setCursor(11,0);
-   lcd_JARED.write(7);
-   delay(100);
-   lcd_JARED.setCursor(5,0);
-   lcd_JARED.write(7);
-   lcd_JARED.setCursor(10,0);
-   lcd_JARED.write(7);
-   delay(100);
-   lcd_JARED.setCursor(7,0);
-   lcd_JARED.write(8);
-   lcd_JARED.setCursor(8,0);
-   lcd_JARED.write(8);
-   delay(100);
     }
     if(Detection == LOW){
-      for (int i = NUM_LEDS - 1; i >= 0; i--) {
+      lcd_JARED.setCursor(7,0);
+      lcd_JARED.print("Detect");
+    for (int i = NUM_LEDS - 1; i >= 0; i--) {
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));  // Apaga el Neopixel
     pixels.show();                                  // Actualiza la rueda de Neopixels
     delay(100);                                     // Espera 100 milisegundos
@@ -393,20 +289,18 @@ void temperaturaMorado(){
    estadoantes1 = estadopush1;
 
    if(estadorele1 == 1){
-    lcd_JARED.clear();
-    lcd_JARED.setCursor(0,0);
-    lcd_JARED.print("1 ON");
-    lcd_JARED.setCursor(4,0);
-    lcd_JARED.write(9);
     digitalWrite(rele1, HIGH);
+    lcd_JARED.clear();
+    lcd_JARED.setCursor(0,1);
+    lcd_JARED.print("L1");
+    lcd_JARED.setCursor(2,1);
+    lcd_JARED.write(9);
    }
    else {
-    lcd_JARED.clear();
-    lcd_JARED.setCursor(0,0);
-    lcd_JARED.print("1 OFF");
-    lcd_JARED.setCursor(5,0);
-    lcd_JARED.write(9);
     digitalWrite(rele1,LOW);
+    lcd_JARED.clear();
+    lcd_JARED.setCursor(0,1);
+    lcd_JARED.print("L1");
    }
   }
   void boton2(){
@@ -418,18 +312,16 @@ void temperaturaMorado(){
    estadoantes2 = estadopush2;
 
    if(estadorele2 == 1){
-    lcd_JARED.setCursor(7,0);
-    lcd_JARED.print("2 ON");
-    lcd_JARED.setCursor(11,0);
-    lcd_JARED.write(9);
     digitalWrite(rele2, HIGH);
+    lcd_JARED.setCursor(4,1);
+    lcd_JARED.print("L2");
+    lcd_JARED.setCursor(6,1);
+    lcd_JARED.write(9);
    }
    else {
-    lcd_JARED.setCursor(7,0);
-    lcd_JARED.print("2 OFF");
-    lcd_JARED.setCursor(12,0);
-    lcd_JARED.write(9);
     digitalWrite(rele2,LOW);
+    lcd_JARED.setCursor(4,1);
+    lcd_JARED.print("L2");
    }
   }
   void boton3(){
@@ -441,21 +333,21 @@ void temperaturaMorado(){
    estadoantes3 = estadopush3;
 
    if(estadorele3 == 1){
-    lcd_JARED.setCursor(0,1);
-    lcd_JARED.print("puerta abierta");
-    lcd_JARED.setCursor(14,1);
+    lcd_JARED.setCursor(8,1);
+    lcd_JARED.print("Door");
+    lcd_JARED.setCursor(12,1);
     lcd_JARED.write(8);
     Motor.write(45); 
-     delay(500);
+     delay(250);
    }
    else {
-    lcd_JARED.setCursor(0,1);
-    lcd_JARED.print("puerta cerrada");
-    lcd_JARED.setCursor(14,1);
+    lcd_JARED.setCursor(8,1);
+    lcd_JARED.print("Door");
+    lcd_JARED.setCursor(12,1);
     lcd_JARED.write(8);
-    lcd_JARED.setCursor(15,1);
+    lcd_JARED.setCursor(13,1);
     lcd_JARED.write(7);
      Motor.write(180);
-     delay(500);
+     delay(250);
    }
   }
